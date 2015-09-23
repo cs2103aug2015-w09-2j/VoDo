ca3994b4ab446d359b11bdaa5bedcb0020eeaf70
tangwr <firebolt2000@hotmail.com>
2015-09-23 17:40:29 +0800

    Update for delete line

--- src/FileStorage.java
+++ src/FileStorage.java
@@ -59,8 +59,29 @@ public class FileStorage {
 		return list;
 	}
 	
-	public void delete(int num, ArrayList<String> dataList){
+	public String delete(String line){
 		
+		String lineDeleted;
+		String fileName = filePath;
+		
+		try{
+			int numLine = Integer.parseInt(line);
+			
+			if(isValidNumLine(numLine, fileName)){
+				lineDeleted = "Invalid line number";
+				System.out.println("Invalid line number");
+			}else{
+				String deletedLine = findLine(numLine, fileName);
+				deleteLine(numLine, fileName);
+				lineDeleted = "deleted from " + fileName + ": \"" + deletedLine + "\"";
+				System.out.println("deleted from " + fileName + ": \"" + deletedLine + "\"");
+			}
+		}catch(NumberFormatException e){
+			lineDeleted = "Invalid line number";
+			System.out.println("Invalid line number");
+		}
+		
+		return lineDeleted;
 	}
 	
 
@@ -218,6 +239,105 @@ public class FileStorage {
 		else{
 			return false;
 		}
+	}
+	
+	private static String findLine(int numLine, String fileName){
+		String line = "";
+		
+		try{
+			FileReader fileReader = new FileReader(fileName);
+			BufferedReader bufferedReader = new BufferedReader(fileReader);
+            
+			for(int i=0; i<numLine; i++){
+				line = bufferedReader.readLine();
+			}
+			bufferedReader.close();   
+			
+			return line;
+			
+		}catch(FileNotFoundException ex) {
+            System.out.println("Unable to open file '" + fileName + "'");                
+        }catch(IOException ex) {
+            System.out.println("Error reading file '" + fileName + "'");                   
+        }
+		
+		return line;
+		
+	}
+	
+	/**
+	 * Open the text file and delete the nth line in the specfic text file
+	 */
+	private static void deleteLine(int numLine, String fileName){
+		//String path = System.getProperty("java.class.path");	
+		File currFile = new File(fileName);
+		File tempFile = new File(currFile.getAbsolutePath() + ".tmp");
+		String line = null;
+		int count = 0;
+		
+		try{
+			FileWriter fileWriter = new FileWriter(tempFile, true);
+            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+			FileReader fileReader = new FileReader(fileName);
+	        BufferedReader bufferedReader = new BufferedReader(fileReader);
+	        
+	        while((line = bufferedReader.readLine()) != null) {
+	        	++count;
+	        	
+				if(count == numLine){
+					continue;
+				}else{
+					 bufferedWriter.write(line);
+					 bufferedWriter.newLine();
+				}
+			}   
+	        bufferedWriter.close();
+	        bufferedReader.close();
+	        
+	        //Delete the original file
+	        if (!currFile.delete()) {
+	          System.out.println("Could not delete file");
+	        } 	        
+	        //Rename the new file to the filename the original file had.
+	        if (!tempFile.renameTo(currFile)){
+	          System.out.println("Could not rename file");
+	        }        
+		}catch(FileNotFoundException ex) {
+            System.out.println("Unable to open file '" + fileName + "'");                
+        }catch(IOException ex) {
+            System.out.println("Error reading file '" + fileName + "'");                   
+        }
+		
+	}
+	
+	private static boolean isValidNumLine(int numLine, String fileName){
+		String line = null;
+		int count = 0;
+		
+		try{
+			FileReader fileReader = new FileReader(fileName);
+
+			BufferedReader bufferedReader = new BufferedReader(fileReader);
+			
+			while((line = bufferedReader.readLine()) != null) {
+				count++;
+			} 
+			
+			bufferedReader.close();  
+			
+			if(count == 0){
+				return true;
+			}
+			else if(numLine > count){
+				return true;
+			}
+			else{
+				return false;
+			}
+		}catch(IOException ex) {
+            System.out.println("Error reading file '" + fileName + "'");                   
+            return true;
+        }
 	}
 	
 	
