d54bbd9d4ffab9563ee97c94ae0c75cea84b866e
Ravi Chandak <Ravi@Ravis-MacBook-Pro.local>
2015-09-27 22:09:01 +0800

    Logic of getting the command and executing command handled

--- src/Logic.java
+++ src/Logic.java
@@ -1,4 +1,37 @@
 
 public class Logic {
+	private static CommandType.Types command; 
+	private static boolean getCommand(String input){
+		command = Parser.parse(input);
+		if(command == CommandType.Types.UNKNOWN){
+			return false;
+		}else{
+		executeCommand(command,input);
+		}
+		return true;
+	}
+	
+	private static boolean executeCommand(CommandType.Types command,String input){
+		switch(command){
+		case ADD_EVENT :
+			break;
+		case ADD_DEADLINE:
+			break;
+		case ADD_FLOATING:
+			break;
+		case UPDATE:
+			break;
+		case DELETE:	
+			break;
+		}
+	return true;	
+	}
 
+	public CommandType.TaskTypes getCommand() {
+		return command;
+	}
+
+	public void setCommand(CommandType.TaskTypes command) {
+		this.command = command;
+	}
 }
