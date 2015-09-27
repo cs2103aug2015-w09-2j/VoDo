3cbd7d00b188e2c2a9c67c27b3881af08d087e18
Ravi Chandak <Ravi@Ravis-MacBook-Pro.local>
2015-09-28 00:28:41 +0800

    execute command method developed further, need to now sync with storage to add it to file

--- src/Logic.java
+++ src/Logic.java
@@ -14,15 +14,25 @@ public class Logic {
 	private static boolean executeCommand(CommandType.Types command,String input){
 		switch(command){
 		case ADD_EVENT :
+			String eventDescription = Parser.getDescription(input.substring(6));
+			DateClass endDate = Parser.getDate(input.substring(6));
+			Event event = new Event(eventDescription,endDate);			
 			break;
 		case ADD_DEADLINE:
+			String deadlineDescription = Parser.getDescription(input.substring(6));
+			DateClass endDeadline = Parser.getDate(input.substring(6));
+			DeadLine deadline = new DeadLine(deadlineDescription,endDeadline);
 			break;
 		case ADD_FLOATING:
+			String floatingDescription = Parser.getDescription(input.substring(6));
+			Floating floating = new Floating(floatingDescription);
 			break;
 		case UPDATE:
 			break;
 		case DELETE:	
 			break;
+		default:
+			break;		
 		}
 	return true;	
 	}
