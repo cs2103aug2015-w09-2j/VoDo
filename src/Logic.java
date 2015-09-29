5d8b7eb6a49370b5a157f8130127394ace6b5021
Ravi Chandak <Ravi@Ravis-MacBook-Pro.local>
2015-09-29 19:55:21 +0800

    getCommand() changed to processCommand() in logic

--- src/Logic.java
+++ src/Logic.java
@@ -2,8 +2,9 @@
 public class Logic {
 	private static FileStorage fileStorage = new FileStorage();
 	private static CommandType.Types command; 
+	
 	@SuppressWarnings("unused")
-	private static boolean getCommand(String input){
+	private static boolean processCommand(String input){
 		command = Parser.parse(input);
 		if(command == CommandType.Types.UNKNOWN){
 			return false;
@@ -14,32 +15,53 @@ public class Logic {
 	}
 	
 	private static boolean executeCommand(CommandType.Types command,String input){
+		boolean success = false;
 		switch(command){
 		case ADD_EVENT :
-			String eventDescription = Parser.getDescription(input.substring(6));
-			DateClass endDate = Parser.getDate(input.substring(6));
-			Event event = new Event(eventDescription,endDate);
+			Event event = createEvent(input);
 			writeTaskTofile(event);
+			success=true;
 			break;
 		case ADD_DEADLINE:
-			String deadlineDescription = Parser.getDescription(input.substring(6));
-			DateClass endDeadline = Parser.getDate(input.substring(6));
-			DeadLine deadline = new DeadLine(deadlineDescription,endDeadline);
+			DeadLine deadline = createDeadLine(input);
 			writeTaskTofile(deadline);
+			success=true;
 			break;
 		case ADD_FLOATING:
-			String floatingDescription = Parser.getDescription(input.substring(6));
-			Floating floating = new Floating(floatingDescription);
+			Floating floating = createFloating(input);
 			writeTaskTofile(floating);
+			success=true;
 			break;
 		case UPDATE:
+			success=true;
 			break;
 		case DELETE:	
+			success=true;
 			break;
 		default:
 			break;		
 		}
-	return true;	
+	return success;	
+	}
+
+	private static Floating createFloating(String input) {
+		String floatingDescription = Parser.getDescription(input.substring(6));
+		Floating floating = new Floating(floatingDescription);
+		return floating;
+	}
+
+	private static DeadLine createDeadLine(String input) {
+		String deadlineDescription = Parser.getDescription(input.substring(6));
+		DateClass endDeadline = Parser.getDate(input.substring(6));
+		DeadLine deadline = new DeadLine(deadlineDescription,endDeadline);
+		return deadline;
+	}
+
+	private static Event createEvent(String input) {
+		String eventDescription = Parser.getDescription(input.substring(6));
+		DateClass endDate = Parser.getDate(input.substring(6));
+		Event event = new Event(eventDescription,endDate);
+		return event;
 	}
 
 	private static void writeTaskTofile(Task task) {
@@ -47,18 +69,33 @@ public class Logic {
 		String writeToFile;
 		switch(taskType){
 		case "Event":
-			writeToFile = taskType + task.getDescription() + ((Event) task).getStartDate().toString() + ((Event) task).getEndDate().toString();
-			fileStorage.write(writeToFile);
+			writeEventToFile(task, taskType);
 			break;
 		case "DeadlLine":
-			writeToFile = taskType + task.getDescription() + ((DeadLine) task).getEndDate().toString();
-			fileStorage.write(writeToFile);
+			writeDeadlineToFile(task, taskType);
 			break;
 		case "Floating":
-			writeToFile = taskType + task.getDescription();
-			fileStorage.write(writeToFile);
+			writeFloatingToFile(task, taskType);
 			break;
 		}
+	}
+
+	private static void writeFloatingToFile(Task task, String taskType) {
+		String writeToFile;
+		writeToFile = taskType + " " +task.getDescription();
+		fileStorage.write(writeToFile);
+	}
+
+	private static void writeDeadlineToFile(Task task, String taskType) {
+		String writeToFile;
+		writeToFile = taskType + " " +task.getDescription() + " " +((DeadLine) task).getEndDate().toString();
+		fileStorage.write(writeToFile);
+	}
+
+	private static void writeEventToFile(Task task, String taskType) {
+		String writeToFile;
+		writeToFile = taskType + " " +task.getDescription()+" " + ((Event) task).getStartDate().toString() +" " + ((Event) task).getEndDate().toString();
+		fileStorage.write(writeToFile);
 	}
 }
 
